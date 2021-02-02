f70c31d30f77bb7e2d8eb23fe8543238465ac3f7
Isabelle Sanford <isabellesanford@gmail.com>
2021-02-01 19:58:16 -0800

    make simple bar in app.js, use csv for data atm

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,37 @@
 
 
 function init() {
-    d3.json('https://isabelle-sanford.github.io/se-data-analysis/datajsons/gamedata.json').then(function(data) {
+    d3.csv("../../resources/SPopulation.csv").then(function(data) {
 
     // SIMPLE BAR:
     // bar(x, y, title, colors, div)
     // colors are blue & red
     // x is dem & republican
     // y is # of people D vs R senators represent
+
+    let dem_total = 0;
+    let rep_total = 0;
+    let other_total = 0;
+
+    data.forEach(d => {
+        let pop = d.Population / 2;
+
+        if (d.Party === "Republican") {
+            rep_total += pop
+        } else if (d.Party === "Democrat") {
+            dem_total += pop
+        } else {
+            other_total += pop
+        }
+    })
+
+    let bar_x = ['Democrat', 'Republican'];
+    let bar_y = [(dem_total + other_total), rep_total];
+    let bar_colors = ['blue', 'red'];
+
+    bar(bar_x, bar_y, '# of People each Party Represents', bar_colors, '#IS-bar');
+
+
 
     // STATE POP BAR:
     // bar_pop_stack(vars1, vars2, div)
@@ -19,9 +43,19 @@ function init() {
 
 
     // CHAMBER PLOT:
+    // chamber_plot(rep_pops, dem_pops, div)
+    // rep_pops is a sorted list of populations from each republican senator
+    // dem_pops is the equivalent, etc
+    // eventually add - hovertext for state / senator
+
+
+
+
+    
+    }).catch(function(error) {
+    console.log(error);
+    });
     
 
 
-
-    });
 }
