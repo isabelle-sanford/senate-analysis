abd932387c1cfd636159b8545da77d9fbfa7e807
Isabelle Sanford <isabellesanford@gmail.com>
2021-02-06 09:58:14 -0800

    background plots color, about page in flask

--- app.py
+++ app.py
@@ -42,35 +42,16 @@ def relig_json():
     # go back to main when done
     return data_jsons.relig_json()
 
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 # more routes to include:
 # other senate reform pages, any other stretch stuff
 
 
-# USEFUL EXAMPLE STUFF-------------------
-# VARIABLES
-# @app.route("/api/v1.0/justice-league/<real_name>")
-# def justice_league_character(real_name):
-#     """Fetch the Justice League character whose real_name matches
-#        the path variable supplied by the user, or a 404 if not."""
-
-#     canonicalized = real_name.replace(" ", "").lower()
-#     for character in justice_league_members:
-#         search_term = character["real_name"].replace(" ", "").lower()
-
-#         if search_term == canonicalized:
-#             return jsonify(character)
-
-#     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
-
-# # Route to render index.html template using data from Mongo
-# @app.route("/")
-# def home():
-
-#     # Find one record of data from the mongo database
-#     destination_data = mongo.db.collection.find_one()
-
-#     # Return template and data
-#     return render_template("index.html", vacation=destination_data)
 
 
 
