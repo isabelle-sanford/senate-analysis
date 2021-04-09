740f9196b2c4ba8db7c2615b4a1089c56524f460
Isabelle Sanford <isabellesanford@gmail.com>
2021-04-09 14:27:56 -0400

    updated original religion pull

--- jsons/religion_cleaning1.py
+++ jsons/religion_cleaning1.py
@@ -5,18 +5,20 @@ data = pd.read_csv('../resources/ReligiousAffiliationByState.csv')  # NOTE: has 
 
 #print(data.head())
 
-data0 = data.replace(to_replace = ['<1', '< 1', '<16', ',', '<!', '<1<1'], value = '0') # regex for 'includes <'?
+data0 = data.replace(to_replace = '^\<*', value = '0', regex = True) # regex for 'includes <'? put in but not tested
+#sprint(data0.head)
 # data1 = data0.replace(to_replace = '< 1', value = '0')
 # data1 = data1.replace(to_replace = '<16', value = '0') # fix in original sheet
 data1 = data0.fillna(value='0')
 
-relig_list = data1.columns
+relig_list = data1.columns # how do you get size of columns? hardcoded 55 is bad
 new_cols = list(range(55))
 data1.columns = new_cols
 
 
 relig_dict = {}
 
+# go through and average over states - NOTE: not best choice bc not accounting for population
 for n in range(1,55):
     try:
         us_perc = round(sum([int(d) for d in list(data1[n])]) / 51, 2)
